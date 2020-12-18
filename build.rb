aa80d18e16e06044065a1e0a9b74380b7304f631
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2020-12-18 18:01:44 +0000

    Now builds multiple packs with differences

--- build.rb
+++ build.rb
@@ -4,7 +4,6 @@ require 'dir'
 require 'fileutils'
 require 'tempfile'
 
-
 def ZipDir(directory, zipfile_name)
   Zip::ZipFile.open(zipfile_name, Zip::ZipFile::CREATE) do |zipfile|
       Dir[File.join(directory, '*')].each do |file|
@@ -17,37 +16,25 @@ def createDir(dir)
   FileUtils.rm_rf Dir.glob("#{dir}/*")
 end
 
-def file_edit(filename, regexp, replacement)
-  tempdir = File.dirname(filename)
-  tempprefix = File.basename(filename)
-  tempprefix.prepend('.') unless RUBY_PLATFORM =~ /mswin|mingw|windows/
-  tempfile =
-    begin
-      Tempfile.new(tempprefix, tempdir)
-    rescue
-      Tempfile.new(tempprefix)
-    end
-  File.open(filename).each do |line|
-    tempfile.puts line.gsub(regexp, replacement)
-  end
-  tempfile.fdatasync unless RUBY_PLATFORM =~ /mswin|mingw|windows/
-  tempfile.close
-  unless RUBY_PLATFORM =~ /mswin|mingw|windows/
-    stat = File.stat(filename)
-    FileUtils.chown stat.uid, stat.gid, tempfile.path
-    FileUtils.chmod stat.mode, tempfile.path
-  else
-    # FIXME: apply perms on windows
-  end
-  FileUtils.mv tempfile.path, filename
+def replaceInFile(filename, originalstring, newstring)
+  # save the content of the file
+  file = File.read(filename)
+  # replace (globally) the search string with the new string
+  new_content = file.gsub(originalstring, newstring)
+  # open the file again and write the new content to it
+  File.open(filename, 'w') { |line| line.puts new_content }
 end
 
-file_edit('/tmp/foo', /foo/, "baz")
+
 createDir("packBuild")
 createDir("BUILD")
 createDir("BUILD/packs")
 
-ZipDir("./packBase/", "packBuild/test.mcpack")
+FileUtils.copy_entry("packBase/", "packBuild/pack")
+ZipDir("./packBuild/pack/", "packBuild/0.mcpack") # Dark nether
+replaceInFile('./packBuild/pack/shaders/glsl/renderchunk.fragment', "vec4 ambientOclusion = vec4(uv1.y + isHell* 0.5);", "vec4 ambientOclusion = vec4(uv1.y + isHell* 2);")
+ZipDir("./packBuild/pack/", "packBuild/1.mcpack") # Light nether
+FileUtils.rm_rf("packBuild/pack")
 FileUtils.copy_entry("packBuild/", "BUILD/packs")
 FileUtils.copy_entry("siteBase/", "BUILD")
 
