f9d7b1db7da2db6fd9c4e925e1b42bb8c2b125a8
OpenSauce04 <danieljamesbradbury@hotmail.com>
2021-01-27 12:26:39 +0000

    I have a square brain

--- siteBase/resources/toggleOptionFlag.js
+++ siteBase/resources/toggleOptionFlag.js
@@ -1,14 +1,16 @@
 function toggleOptionFlag(pos, disabled, enabled) {
-  if (typeof disabled === 'undefined') { optionalArg = '0'; }
-  if (typeof enabled === 'undefined') { optionalArg = '1'; }
+  if (typeof disabled === 'undefined') { disabled = '0'; }
+  if (typeof enabled === 'undefined') { enabled = '1'; }
   
   if (pageid.substring(pos,pos+1) == enabled) {
     newpageid=pageid
     newpageid[pos]=disabled
     window.history.pushState("", "", newpageid)
+    console.log("ye")
   } else {
     newpageid=pageid
     newpageid[pos]=enabled
     window.history.pushState("", "", newpageid)
+    console.log("ye")
   }
 }
