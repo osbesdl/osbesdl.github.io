826153e680d3c17d71b4e8b32431a838c17bc959
OpenSauce04 <danieljamesbradbury@hotmail.com>
2021-01-27 12:09:21 +0000

    Flag toggle characters can now be customized

--- siteBase/resources/toggleOptionFlag.js
+++ siteBase/resources/toggleOptionFlag.js
@@ -1,11 +1,14 @@
-function toggleOptionFlag(pos, ) {
-  if (pageid.substring(pos,pos+1) == 1) {
+function toggleOptionFlag(pos, disabled, enabled) {
+  if (typeof disabled === 'undefined') { optionalArg = '0'; }
+  if (typeof enabled === 'undefined') { optionalArg = '1'; }
+  
+  if (pageid.substring(pos,pos+1) == enabled) {
     newpageid=pageid
-    newpageid[pos]=0
+    newpageid[pos]=disabled
     window.history.pushState("", "", newpageid)
   } else {
     newpageid=pageid
-    newpageid[pos]=1
+    newpageid[pos]=enabled
     window.history.pushState("", "", newpageid)
   }
 }
