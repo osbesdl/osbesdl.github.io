2f6c629f6b8fc6da305dc43d8027d7f9776e558c
OpenSauce04 <danieljamesbradbury@hotmail.com>
2021-01-27 11:50:34 +0000

    ID is no longer always 'bn'

--- siteBase/resources/packOption.js
+++ siteBase/resources/packOption.js
@@ -1,3 +1,3 @@
 function packOption(name, id, defaultstate) {
-  document.write(name + ' <span class="checkboxes"><a onclick="boxclicked(\''+ id +'\')"><img style="cursor: pointer;" id="bn" src="resources/'+ defaultstate +'.png"></img></a></span>' + info(id) + '<br>')
+  document.write(name + ' <span class="checkboxes"><a onclick="boxclicked(\''+ id +'\')"><img style="cursor: pointer;" id='+ id +' src="resources/'+ defaultstate +'.png"></img></a></span>' + info(id) + '<br>')
 }
