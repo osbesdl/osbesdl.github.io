ab4738eed38c5ea2529572c9c4b48af9e5294bd0
OpenSauce04 <danieljamesbradbury@hotmail.com>
2021-06-13 17:09:20 +0100

    Added older versions button

--- SITE/resources/downloads.js
+++ SITE/resources/downloads.js
@@ -2,7 +2,7 @@ function downloadPack(version) {
   window.location.href = './packs/OSBESv' + version + '.mcpack'
 }
 function pack(version, badge) {
-  output='<a onclick="downloadPack(\'' + version + '\')"><img src="resources/download.png" width="3%"></img></a> OSBES v' + version
+  output='<a onclick="downloadPack(\'' + version + '\')"><img src="resources/download.png" width="8%"></img></a> OSBES v' + version
   if (badge == "latest") {
     output+=' <img src="resources/badges/latest.svg" height="5%"></img>'
   }
@@ -11,3 +11,14 @@ function pack(version, badge) {
 
 // Pack definitions
 pack("0.12.3", "latest");
+document.write("<div class='hidden' id='hidden'>");
+pack("0.12.2");
+document.write("</div>");
+
+function showHiddenPacks() {
+  var hiddendiv = document.getElementById("hidden");
+  hiddendiv.style.display = 'block';
+
+  var showhiddendiv = document.getElementById("showhidden");
+  showhiddendiv.style.display = 'none';
+}
