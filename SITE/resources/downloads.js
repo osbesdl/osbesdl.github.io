c879fbea2d1781d76166d11aa42b51500f1a776a
OpenSauce04 <danieljamesbradbury@hotmail.com>
2021-06-14 12:24:42 +0100

    Adjusted badge size

--- SITE/resources/downloads.js
+++ SITE/resources/downloads.js
@@ -2,7 +2,7 @@ function pack(version, badge) {
   if (version!="Experimental") {
     output='<a href="./packs/OSBESv' + version + '.mcpack"><img src="resources/download.png" class="download"></img> OSBES v' + version;
   } else {
-    output='<a href="https://github.com/jebbyk/OSBES-minecraft-bedrock-edition-shader/archive/refs/heads/develop.zip"><img src="resources/download.png" class="download"></img> OSBES ' + version;
+    output='<a href="https://github.com/jebbyk/OSBES-minecraft-bedrock-edition-shader/archive/refs/heads/develop.zip" class="experimental"><img src="resources/download.png" class="download"></img> OSBES ' + version;
   }
   switch (badge) {
     case "latest": output+=' <img class="badge" src="resources/badges/latest.svg"></img>'; break
